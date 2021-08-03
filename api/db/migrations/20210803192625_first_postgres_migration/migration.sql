8d81c28d95831ad059089f9eb64046e559bc95b9
Pavol Gurican <pavol@gurican.sk>
2021-08-03 21:36:20 +0200

    switch sqlite for postgresql

---
+++ api/db/migrations/20210803192625_first_postgres_migration/migration.sql
@@ -0,0 +1,32 @@
+-- CreateTable
+CREATE TABLE "UserExample" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserExample.email_unique" ON "UserExample"("email");
--- api/src/services/posts/posts.js
+++ api/src/services/posts/posts.js
@@ -17,12 +17,14 @@ export const post = ({ id }) => {
 }
 
 export const createPost = ({ input }) => {
+  requireAuth()
   return db.post.create({
     data: input,
   })
 }
 
 export const updatePost = ({ id, input }) => {
+  requireAuth()
   return db.post.update({
     data: input,
     where: { id },
@@ -30,7 +32,13 @@ export const updatePost = ({ id, input }) => {
 }
 
 export const deletePost = ({ id }) => {
+  requireAuth()
   return db.post.delete({
     where: { id },
   })
+}
+
+export const Post = {
+  user: (_obj, { root }) =>
+    db.post.findUnique({ where: { id: root.id } }).user(),
 }
